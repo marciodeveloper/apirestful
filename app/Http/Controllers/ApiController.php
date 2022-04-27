9ae257660c770e814cf0b88dc6bf514e35bea734
marciodeveloper <marciodeveloper@outlook.com>
2022-04-27 12:52:52 -0300

    feats: Create Student method

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,15 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Student;
 
-class APiController extends Controller
+class ApiController extends Controller
 {
     public function getAllStudents() {
+
+
 
     }
 
     public function createStudent(Request $request) {
         
+        $student = new Student;
+        $student->name = $request->name;
+        $student->course = $request->course;
+        $student->save();
+
+        return response()->json([
+            "message" => "student record created"
+        ], 201);
+
     }
 
     public function getStudent($id) {
